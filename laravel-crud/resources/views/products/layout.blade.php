b152d87c8f1adcdda0414402c6e6c8d30b0e5090
Maximus-c <75832261+Maximus-c@users.noreply.github.com>
2022-11-07 14:44:25 +0100

    message

---
+++ laravel-crud/resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Crud Application</title>
+    <link href="https://bootswatch.com/5/slate/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+   <h1>Laravel CRUD</h1>
+    <div class="container">
+        @yield('content')
+    </div> 
+
+</body>
+</html>
